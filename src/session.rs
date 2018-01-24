0658ef8bdf2179a929ebf2dcda6f82bd2b3ee26d
James Forcier <james.forcier@coreos.com>
2018-01-23 16:36:04 -0800

    session: implement Drop for Session

--- src/session.rs
+++ src/session.rs
@@ -1,6 +1,7 @@
 use errors::*;
+use types::*;
 
-use yubihsm_sys::{self, yh_session, yh_rc_YHR_SUCCESS};
+use yubihsm_sys::{self, yh_session};
 
 use std::cell::Cell;
 
@@ -13,6 +14,24 @@ impl Session {
     pub(crate) fn new(this: *mut yh_session) -> Session {
         Session {
             this: Cell::new(this),
+        }
+    }
+}
+
+impl Drop for Session {
+    fn drop(&mut self) {
+        unsafe {
+            let rc = ReturnCode::from(yubihsm_sys::yh_util_close_session(self.this.get()));
+
+            if rc != ReturnCode::Success {
+                panic!("failed to close session: {}", rc);
+            }
+
+            let rc = ReturnCode::from(yubihsm_sys::yh_destroy_session(&mut self.this.get()));
+
+            if rc != ReturnCode::Success {
+                panic!("failed to destroy session: {}", rc);
+            }
         }
     }
 }
