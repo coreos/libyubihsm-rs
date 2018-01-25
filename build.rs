13588e2680501196aa32e4809ea4edba0ce88138
James Forcier <james.forcier@coreos.com>
2018-01-25 11:53:25 -0800

    build: cargo fmt

--- build.rs
+++ build.rs
@@ -20,6 +20,7 @@ fn main() {
         .expect("Unable to generate bindings");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings.write_to_file(out_path.join("bindings.rs"))
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 }
