8f4e057c8eba32a28e99b098b5089e9c9effa496
James Forcier <james.forcier@coreos.com>
2018-02-23 16:44:53 -0800

    session: add set_log_index()

--- src/session.rs
+++ src/session.rs
@@ -568,6 +568,18 @@ impl Session {
             log_entries: entries.iter().map(LogEntry::from).collect::<Vec<_>>(),
         })
     }
+
+    pub fn set_log_index(&self, log_index: u16) -> Result<(), Error> {
+        unsafe {
+            match ReturnCode::from(yubihsm_sys::yh_util_set_log_index(
+                self.this.get(),
+                log_index,
+            )) {
+                ReturnCode::Success => Ok(()),
+                e => bail!("util_set_log_index failed: {}", e),
+            }
+        }
+    }
 }
 
 impl Drop for Session {
