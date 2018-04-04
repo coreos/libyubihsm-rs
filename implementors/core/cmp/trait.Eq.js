2122a65fd31f3cf505afbb8de7556e2c50e08e41
James Forcier <james.forcier@coreos.com>
2018-04-03 17:21:19 -0700

    (cargo-release) generate docs

---
+++ implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["libyubihsm"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"libyubihsm/struct.Domain.html\" title=\"struct libyubihsm::Domain\">Domain</a>",synthetic:false,types:["libyubihsm::types::Domain"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"libyubihsm/enum.PublicKey.html\" title=\"enum libyubihsm::PublicKey\">PublicKey</a>",synthetic:false,types:["libyubihsm::types::PublicKey"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"libyubihsm/enum.Command.html\" title=\"enum libyubihsm::Command\">Command</a>",synthetic:false,types:["libyubihsm::types::Command"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"libyubihsm/enum.CommandType.html\" title=\"enum libyubihsm::CommandType\">CommandType</a>",synthetic:false,types:["libyubihsm::types::CommandType"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/fmt/trait.Display.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["libyubihsm"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"libyubihsm/enum.ReturnCode.html\" title=\"enum libyubihsm::ReturnCode\">ReturnCode</a>",synthetic:false,types:["libyubihsm::types::ReturnCode"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"libyubihsm/enum.Algorithm.html\" title=\"enum libyubihsm::Algorithm\">Algorithm</a>",synthetic:false,types:["libyubihsm::types::Algorithm"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
